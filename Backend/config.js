69442e950dae8c8c6a32d8c94f342da24bf6c285
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-04 18:14:54 +0530

    Create config.js

---
+++ Backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://suhas:Ravan2020@book-store-mern.vujgyut.mongodb.net?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
