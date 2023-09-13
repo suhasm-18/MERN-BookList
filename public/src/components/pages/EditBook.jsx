2414c9af6c9d4707d97cbd8c1e0ad263e146573a
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-13 12:05:08 +0530

    Update EditBook.jsx

--- public/src/components/pages/EditBook.jsx
+++ public/src/components/pages/EditBook.jsx
@@ -72,4 +72,25 @@ useEffect(() => {
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-          
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='number'
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default EditBook
+
