34c12f9e406672f3cf9b487221ca0eb1842ee1fa
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-13 12:01:48 +0530

    Create EditBook.jsx

---
+++ public/src/components/pages/EditBook.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const EditBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  const { enqueueSnackbar } = useSnackbar();
